6e10661e393c7b66a93c3b1940743a5913411b37
Stephen <stephen.stewart@carisenda.com>
2017-08-08 10:36:23 +0100

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var Serializers = require('./lib/cache-serializers.js');
-
-var MedeaSerializer = Serializers.MedeaDbSerializer;
-
-var serializer = new MedeaSerializer({ cacheDirPath: './medea-cache'});
-
-serializer.read()
-serializer.write([{key: 'foo', value: 'foo'}, {key: 'bar', value: null}]);
